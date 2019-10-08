331dd510b8f1e32428eca812d49adbe602a37623
Irvin <irvinalcira@gmail.com>
2019-10-08 11:40:31 -0700

    fixed bug

--- App.js
+++ App.js
@@ -3,21 +3,22 @@ import React from 'react';
 import {
   View,
   Text,
+  ScrollView
 } from 'react-native';
 import About from './comps/About';
 import Contacts from './comps/Contacts';
+import Main from './comps/Main';
+
 
 function App(){
   return (
-    <View style={styles.app}>
-    
-      <Text>Welcome to the Site!</Text>
+    <ScrollView>
       <Main />
 
       <About />
       <Contacts/>
       
-    </View>
+    </ScrollView>
 
   )
 };
--- comps/Main.js
+++ comps/Main.js
@@ -1,20 +1,35 @@
 import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
-import Logo from './imgs/dog.JPG';
+import {View, Text, Button, Image} from 'react-native';
+import Logo from '../imgs/dog.jpg';
 
 function Main(){
     const [mainText, setMainText] = useState("");
     const [mainStyle, setMainStyle] = useState({
-        backgroundColor:"#C9D5D4"
+ 
     });
     const [textSize, setTextSize] = useState(12);
 
     return (
         <View style={mainStyle}>
-            return <img src={Logo} alt="Logo" />;
+    
+          <Image
+            style={{width:200, 
+                marginTop:30,
+                height:200, 
+                justifyContent:"center", 
+                alignItems:"center", 
+                alignContent:"center",
+                marginLeft:90
+            }}
+            source={require('../imgs/dog.jpg')}
+            />
+
+            <Text style={{justifyContent:"center", textAlign:"center",
+        paddingTop:20}}>Welcome to our site!</Text>
+
         </View>
 
-    )
+    );
 }
 
 export default Main;
